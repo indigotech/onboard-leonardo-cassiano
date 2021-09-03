3137e958867b22bf4684f3034b2c5265cef8f7df
Taqtile <taqtile@Demoiselle.local>
2021-09-03 15:51:22 -0300

    Login screen Frontend step1/2

--- hello/App.tsx
+++ hello/App.tsx
@@ -1,43 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  TextInput,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 const Section: React.FC<{
   title: string;
 }> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
       <Text
         style={[
           styles.sectionTitle,
           {
-            color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
         {title}
@@ -46,7 +26,6 @@ const Section: React.FC<{
         style={[
           styles.sectionDescription,
           {
-            color: isDarkMode ? Colors.light : Colors.dark,
           },
         ]}>
         {children}
@@ -55,24 +34,40 @@ const Section: React.FC<{
   );
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const onLogin = async()=>{
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+
+};
+
+const App = () => {
+  const [email, onChangeEmail] = React.useState("");
+  const [password, onChangePassword] = React.useState("");
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Primeiro programa">
-            Hello Wolrd!
+    <SafeAreaView>
+        <View>
+          <Section title="Bem-vindo(a) à Taqtile!">
           </Section>
+          <Text>
+            E-mail
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText = {onChangeEmail}
+            value = {email}
+          />
+          <Text>
+            Senha
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText = {onChangePassword}
+            value = {password}
+          />
+          <Button onPress={onLogin} title= "Entrar">
+          </Button>
         </View>
-      
     </SafeAreaView>
   );
 };
@@ -93,6 +88,19 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  button: {
+    height: 30,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
