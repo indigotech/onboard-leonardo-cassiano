24cc21079113d6253f086d284193be1c80a4d689
Taqtile <taqtile@Demoiselle.local>
2021-09-09 08:50:17 -0300

    Prettier

--- hello/App.tsx
+++ hello/App.tsx
@@ -1,89 +1,47 @@
 import React from 'react';
-import {
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Alert,
-} from 'react-native';
-
+import { Button, SafeAreaView, StyleSheet, Text, View, TextInput, Alert } from 'react-native';
 
 const Section: React.FC<{
   title: string;
-}> = ({children, title}) => {
+}> = ({ children, title }) => {
   return (
     <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-          },
-        ]}>
-        {children}
-      </Text>
+      <Text style={[styles.sectionTitle, {}]}>{title}</Text>
+      <Text style={[styles.sectionDescription, {}]}>{children}</Text>
     </View>
   );
 };
 
-const onLogin = (email: string, password: string)=>{
-
-  const validEmailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
+const onLogin = (email: string, password: string) => {
+  const validEmailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
   const validPasswordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
 
-  if ( validEmailRegex.test(email) && validPasswordRegex.test(password)) {
-    console.log('TODO')
-  }
-  else if(!validEmailRegex.test(email) && validPasswordRegex.test(password)) {
+  if (validEmailRegex.test(email) && validPasswordRegex.test(password)) {
+    console.log('TODO');
+  } else if (!validEmailRegex.test(email) && validPasswordRegex.test(password)) {
     Alert.alert('Email inválido');
-  }
-  else if(validEmailRegex.test(email) && !validPasswordRegex.test(password)){
+  } else if (validEmailRegex.test(email) && !validPasswordRegex.test(password)) {
     Alert.alert('Senha inválida');
-  }
-  else {
+  } else {
     Alert.alert('Email e Senha inválidos');
   }
-
 };
 
 const App = () => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
   return (
     <SafeAreaView>
-        <View>
-          <Section title="Bem-vindo(a) à Taqtile!"/>
-          <Text>
-            E-mail
-          </Text>
-          <TextInput
-            style={styles.input}
-            onChangeText = {setEmail}
-            value = {email}
-          />
-          <Text>
-            Senha
-          </Text>
-          <TextInput
-            style={styles.input}
-            onChangeText = {setPassword}
-            value = {password}
-          />
-          <Button onPress={() => onLogin(email, password)} 
-                  title= "Entrar"
-          />
-        </View>
+      <View>
+        <Section title='Bem-vindo(a) à Taqtile!' />
+        <Text>E-mail</Text>
+        <TextInput style={styles.input} onChangeText={setEmail} value={email} />
+        <Text>Senha</Text>
+        <TextInput style={styles.input} onChangeText={setPassword} value={password} />
+        <Button onPress={() => onLogin(email, password)} title='Entrar' />
+      </View>
     </SafeAreaView>
   );
 };
@@ -105,7 +63,7 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
-  
+
   input: {
     height: 40,
     margin: 12,
