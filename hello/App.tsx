d56e24196e84b55fb947418464e0406e5f651e96
Taqtile <taqtile@Demoiselle.local>
2021-09-13 11:11:18 -0300

    Apollo client working

--- hello/App.tsx
+++ hello/App.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
 import { Button, SafeAreaView, StyleSheet, Text, View, TextInput, Alert } from 'react-native';
+import { ApolloClient, ApolloProvider, gql, InMemoryCache, useMutation } from '@apollo/client';
+
+const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(data: { email: $email, password: $password }) {
+      token
+      user {
+        id
+        birthDate
+        name
+        role
+      }
+    }
+  }
+`;
+
+const client = new ApolloClient({
+  uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
+  cache: new InMemoryCache(),
+});
 
 const Section: React.FC<{
   title: string;
@@ -12,9 +32,26 @@ const Section: React.FC<{
   );
 };
 
+const Title: React.FC<{ title: string }> = () => {
+  const [mutate, { loading, data, error }] = useMutation(LOGIN, {
+    variables: {
+      email: 'admin@taqtile.com.br',
+      password: '1234qwer',
+    },
+  });
+
+  return (
+    <>
+      <Button onPress={() => mutate().then(console.log).catch(console.error)} title='Logar' />
+      <Text> Loading: {loading && "LOADING"}</Text>
+      <Text> Data: {data && data.toString()}</Text>
+      <Text> Error: {error && error.toString()}</Text>
+    </>
+  );
+};
+
 const onLogin = (email: string, password: string) => {
-  const validEmailRegex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
+  const validEmailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
   const validPasswordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
 
   if (validEmailRegex.test(email) && validPasswordRegex.test(password)) {
@@ -33,16 +70,19 @@ const App = () => {
   const [password, setPassword] = React.useState('');
 
   return (
-    <SafeAreaView>
-      <View>
-        <Section title='Bem-vindo(a) à Taqtile!' />
-        <Text>E-mail</Text>
-        <TextInput style={styles.input} onChangeText={setEmail} value={email} />
-        <Text>Senha</Text>
-        <TextInput style={styles.input} onChangeText={setPassword} value={password} />
-        <Button onPress={() => onLogin(email, password)} title='Entrar' />
-      </View>
-    </SafeAreaView>
+    <ApolloProvider client = {client}>
+      <SafeAreaView>
+        <View>
+          <Section title='Bem-vindo(a) à Taqtile!' />
+          <Text>E-mail</Text>
+          <TextInput style={styles.input} onChangeText={setEmail} value={email} />
+          <Text>Senha</Text>
+          <TextInput style={styles.input} onChangeText={setPassword} value={password} />
+          <Button onPress={() => onLogin(email, password)} title='Entrar' />
+          <Title title='Logar' />
+        </View>
+      </SafeAreaView>
+    </ApolloProvider>
   );
 };
 
