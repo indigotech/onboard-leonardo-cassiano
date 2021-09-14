c9f69d19152fd0f33b84495987e783582ca6e7bb
Taqtile <taqtile@Demoiselle.local>
2021-09-14 10:37:03 -0300

    Pushing to rebase

--- hello/App.tsx
+++ hello/App.tsx
@@ -1,76 +1,70 @@
 import React from 'react';
-import { Button, SafeAreaView, StyleSheet, Text, View, TextInput, Alert } from 'react-native';
-/**import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apollo/client";
-import { render } from "react-dom";
-
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache()
-});
-
-function ExchangeRates() {
-  const { loading, error, data } = useQuery(gql`
-    {
-      rates(currency: "USD") {
-        currency
-        rate
-      }
-    }
-  `);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data.rates.map(({ currency, rate }) => (
-    <div key={currency}>
-      <p>
-        {currency}: {rate}
-      </p>
-    </div>
-  ));
-}**/
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TextInput,
+} from 'react-native';
 
 const Section: React.FC<{
   title: string;
-}> = ({ children, title }) => {
+}> = ({children, title}) => {
   return (
     <View style={styles.sectionContainer}>
-      <Text style={[styles.sectionTitle, {}]}>{title}</Text>
-      <Text style={[styles.sectionDescription, {}]}>{children}</Text>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+          },
+        ]}>
+        {children}
+      </Text>
     </View>
   );
 };
+  
+const onLogin = async()=>{
 
-const onLogin = (email: string, password: string) => {
-  const validEmailRegex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
-  const validPasswordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
-
-  if (validEmailRegex.test(email) && validPasswordRegex.test(password)) {
-    console.log('TODO');
-  } else if (!validEmailRegex.test(email) && validPasswordRegex.test(password)) {
-    Alert.alert('Email inválido');
-  } else if (validEmailRegex.test(email) && !validPasswordRegex.test(password)) {
-    Alert.alert('Senha inválida');
-  } else {
-    Alert.alert('Email e Senha inválidos');
-  }
 };
 
 const App = () => {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
 
   return (
     <SafeAreaView>
-      <View>
-        <Section title='Bem-vindo(a) à Taqtile!' />
-        <Text>E-mail</Text>
-        <TextInput style={styles.input} onChangeText={setEmail} value={email} />
-        <Text>Senha</Text>
-        <TextInput style={styles.input} onChangeText={setPassword} value={password} />
-        <Button onPress={() => onLogin(email, password)} title='Entrar' />
-      </View>
+        <View>
+          <Section title="Bem-vindo(a) à Taqtile!"/>
+          <Text>
+            E-mail
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText = {setEmail}
+            value = {email}
+          />
+          <Text>
+            Senha
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText = {setPassword}
+            value = {password}
+          />
+          <Button onPress={onLogin} title= "Entrar" />
+        </View>
     </SafeAreaView>
   );
 };
