1dd88f432d5aa76e76da268aada58e3c0387039c
Leonardo-Cassiano <37748535+Leonardo-Cassiano@users.noreply.github.com>
2021-09-16 15:53:27 -0300

    Simplifying conditions

--- hello/src/validator.ts
+++ hello/src/validator.ts
@@ -1,19 +1,11 @@
 export const emailvalidator = (email: string) => {
   const validEmailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-  if (validEmailRegex.test(email)) {
-    return true;
-  } else {
-    return false;
-  }
+  return validEmailRegex.test(email)
 };
 
 export const passwordValidator = (password: string) => {
   const validPasswordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
 
-  if (validPasswordRegex.test(password)) {
-    return true;
-  } else {
-    return false;
-  }
+  result validPasswordRegex.test(password)
 };
