770ac4b9c291e73091e862d7e43e7ea84a998e10
Leonardo-Cassiano <leocassianoure@gmail.com>
2021-09-22 09:53:09 -0300

    Red input fields

--- hello/src/components/input-section.tsx
+++ hello/src/components/input-section.tsx
@@ -1,15 +1,26 @@
-import { StyledInput, StyledText } from '../styles';
+import { StyledInput, StyledText, StyledErrorText, StyledErrorInput } from '../styles';
 import React, { ComponentState } from 'react';
 
-export const InputSection: React.FC<{ label: string; input: string; onChange: ComponentState }> = ({
+export const InputSection: React.FC<{ label: string; input: string; onChange: ComponentState; onError: boolean }> = ({
   label,
   input,
   onChange,
+  onError,
 }) => {
   return (
     <>
-      <StyledText>{label}</StyledText>
-      <StyledInput onChangeText={onChange} value={input} />
+      {onError ? (
+        <>
+          <StyledText>{label}</StyledText>
+          <StyledErrorInput onChangeText={onChange} value={input} />
+          <StyledErrorText color='red'>{label} está incorreto</StyledErrorText>
+        </>
+      ) : (
+        <>
+          <StyledText>{label}</StyledText>
+          <StyledInput onChangeText={onChange} value={input} />
+        </>
+      )}
     </>
   );
 };
